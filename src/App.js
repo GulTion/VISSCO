3f6fcb6b8605094ba249769afcafd03cee6b427b
GulTion <gkv8192@gmail.com>
2021-11-19 14:58:00 +0530

    go

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+import PeerMake from "./components/PeerMake";
+import RemoteList from "./components/RemoteList";
+import logo from "./logo.svg";
+import store from "./store/store";
+import "./_App.scss";
+
+function App() {
+  useEffect(() => {
+    console.log(store);
+  });
+  return (
+    <div className="App">
+      <PeerMake />
+      <h1>Hello</h1>
+      {/* <RemoteList /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Editor/Editor.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+
+import './Editor.scss'
+
+
+export default function Header() {
+  const [content, saveContent] =
+    React.useState('Using Python as backenlowed in Javascript, for example disk access. Click button below to save this content to hard drive.')
+
+  return (
+    <div className='editor-container'>
+      <textarea className='textarea' value={content} onChange={(e) => {
+        saveContent(e.target.value)
+      }}/><br/>
+
+      <button className='button' onClick={() => {
+        window.pywebview.api.save_content(content)
+      }}>Save</button>
+    </div>
+  )
+}

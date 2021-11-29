68a46aa5ed700b37a0b6fe4987ff10f91450ecb1
9R48H4 <prabhabatra2112@gmail.com>
2021-11-28 18:04:05 -0800

    sohowing fo how tocommit

--- src/index.py
+++ src/index.py
@@ -4,29 +4,27 @@ import webview
 from pynput.mouse import Controller
 
 mice = Controller()
+# $ git config --global user.name "John Doe"
+# $ git config --global user.email johndoe@example.com
+
 
 class Api:
     def handleMouse(self, data):
-        if(data["type"]=="move"):
-            mice.position = (data["data"]["x"],data["data"]["y"] )
-
-
+        if(data["type"] == "move"):
+            mice.position = (data["data"]["x"], data["data"]["y"])
+            # cdodnsfn f
 
             # mouse.position = (data["data"]["x"], data["data"]["y"])
-        
-        
-
-            
 
 
 def get_entrypoint():
     def exists(path):
         return os.path.exists(os.path.join(os.path.dirname(__file__), path))
 
-    if exists('../gui/index.html'): # unfrozen development
+    if exists('../gui/index.html'):  # unfrozen development
         return '../gui/index.html'
 
-    if exists('../Resources/gui/index.html'): # frozen py2app
+    if exists('../Resources/gui/index.html'):  # frozen py2app
         return '../Resources/gui/index.html'
 
     if exists('./gui/index.html'):
@@ -35,12 +33,11 @@ def get_entrypoint():
     raise Exception('No index.html found')
 
 
-
-
-
 entry = get_entrypoint()
 
 # @set_interval(1)
+
+
 def update_ticker():
     if len(webview.windows) > 0:
         webview.windows[0].evaluate_js("let a = 3")

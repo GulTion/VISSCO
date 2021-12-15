94dd517bff93fdfb98d8818c9521ea87cdf0ec9e
GulTion <gkv8192@gmail.com>
2021-12-15 13:23:19 +0530

    ok

--- src/utils/configs.js
+++ src/utils/configs.js
@@ -29,6 +29,6 @@ const myapi = {
 document.myapi = myapi;
 
 export const KEY_CONFIG = { string: "13", length: 2 };
-// export const SOCKET_URL = "https://chitraBackend.gultion.repl.co";
+export const SOCKET_URL = "https://chitraBackend.gultion.repl.co";
 // export const SOCKET_URL = "http://127.0.0.1:5000/";
-export const SOCKET_URL = "https://kind-owl-67.loca.lt/";
+// export const SOCKET_URL = "https://kind-owl-67.loca.lt/";

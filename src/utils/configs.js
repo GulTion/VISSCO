23cdda4b3e211fc5a5d33ea0de438f4b6d56fb68
GulTion <gkv8192@gmail.com>
2021-12-31 11:07:41 +0530

    push

--- src/utils/configs.js
+++ src/utils/configs.js
@@ -47,7 +47,7 @@ document.myapi = myapi;
 
 export const KEY_CONFIG = { string: "13", length: 2 };
 // export const SOCKET_URL = "https://chitraBackend.gultion.repl.co";
-// export const SOCKET_URL = "http://127.0.0.1:5000/";
+export const SOCKET_URL = "http://127.0.0.1:5000/";
 // export const SOCKET_URL = "https://kind-owl-67.loca.lt/";
-export const SOCKET_URL = "https://miniServer.gultion.repl.co";
+// export const SOCKET_URL = "https://miniServer.gultion.repl.co";
 // export const

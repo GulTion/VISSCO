fe9df7f32ca198e52f48d563d5ab18542a4e38e8
GulTion <gkv8192@gmail.com>
2022-01-10 12:28:19 +0530

    done

--- src/index.py
+++ src/index.py
@@ -1,40 +1,75 @@
 import os
+
 import webview
 
-from pynput.mouse import Controller
-from pynput.keyboard import Key, Controller
-keyboard = Controller()
+from pynput.mouse import Controller as MC
+from pynput.keyboard import Controller as KC
+from pynput.keyboard import Key
+from pynput.mouse import Button
+
+mice = MC()
+kbd = KC()
+
+
+def map(js):
+    return mapper[js]
 
 
 class Api:
+    def handleMouse(self, data):
+        print(data)
+        if(data["data"] == "MOVE"):
+            # if()
+            mice.position = (data["info"]["x"], data["info"]["y"])
+
     def handleMouse(self, pac):
         print(pac)
-        if(pac["data"]=="MOVE"):
-            print("move")
-        elif(pac["data"]=="CLICK"):
-            print("click")
-            if(pac["key"]=="LEFT"):
-                print("left click")
-            elif(pac["key"]=="RIGHT"):
-                print("right click")    
+        if(pac["data"] == "MOVE"):
+            # print("move")
+            mice.position = (pac["info"]["x"], pac["info"]["y"])
+        elif(pac["data"] == "CLICK"):
+            # print("click")
+            if(pac["key"] == "LEFT"):
+                mice.click(Button.left, 1)
 
-        pass
+                # print("left click")
+            elif(pac["key"] == "RIGHT"):
+                mice.click(Button.right, 1)
+
+                # print("right click")
 
     def handleKeyboard(self, data):
+        # data = {
+        #     key: {key: "D", has: 0/1  # data["key"]["has"]
+        #           },
+        #     type: "KEYBOARD",
+        #     data: "DOWN/UP"  # data["data"]
+        # }
         print(data)
-<<<<<<< HEAD
+        key = data["key"]["key"]
+        if(data["data"] == "DOWN"):
+            if(data["key"]["has"] == 0):
+                kbd.press(key)
+                # print("KEy 0 type down")
+            elif(data["key"]["has"] == 1):
+                kbd.press(Key[key])
+                # print("KEy 1 type down")
 
-=======
-        if(data["key"]=="DOWN"):
-           print("DOWN")
-        elif(data["key"]=="UP"):
-           print("UP")
-           
-        pass
->>>>>>> 3c72d5f321b8ebcb9f58dccaa40f6290f6a92821
+        elif(data["data"] == "UP"):
+            if(data["key"]["has"] == 0):
+                kbd.release(key)
+
+                # print("KEy 0 type up")
+            elif(data["key"]["has"] == 1):
+                kbd.release(Key[key])
+                # print("KEy 1 type up")
+
 
 def get_entrypoint():
-    def exists(path):zen development
+    def exists(path):
+        return os.path.exists(os.path.join(os.path.dirname(__file__), path))
+
+    if exists('../gui/index.html'):  # unfrozen development
         return '../gui/index.html'
 
     if exists('../Resources/gui/index.html'):  # frozen py2app
